73985ae7e0bca4bea95c03b66bd45ad69106fbe9
DEVELOPER <developer@localhost.localdomain>
2023-07-05 09:58:16 +0300

    first commit

---
+++ app/Controllers/SiteController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Controllers;
+
+class SiteController extends BaseController {
+    public function simple()
+    {
+        echo "This is a very simple program of controllaer + route";
+    }
+}
